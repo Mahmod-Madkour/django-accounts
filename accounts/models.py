8acdc0b7f5a6a34ef3c8704287635cd538c4a2a2
M_Madkour <mahhmoudmadkour@gmail.com>
2024-01-10 21:50:17 +0200

    add create profile signal

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -18,7 +20,14 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=15, null=False, blank=True)
     last_name = models.CharField(max_length=15, null=False, blank=True)
     email = models.CharField(max_length=15, null=False, blank=True)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=False, blank=True)
+    #country = models.CharField(max_length=15, null=False, blank=True)
+    #city = models.CharField(max_length=15, null=False, blank=True)
 
     def __str__(self):
         return str(self.user)
+
+@receiver(post_save, sender=User)
+def craete_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user = instance)
